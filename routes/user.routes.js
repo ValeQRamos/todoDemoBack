4cd4b7483c11fd073e9e9b139a66b579e2d2290e
Vale Quiroga <valentinqramos@outlook.com>
2022-08-15 20:19:51 -0500

    user routes complete

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,18 +1,28 @@
 const router = require("express").Router();
-//importar el controlador 
-const {getLoggedUser,editProfile} = require("../controllers/user.controller")
-//vamos a importar los middleware 
-const { verifyToken } = require("../middleware")
+//importar el controlador
+const {
+  getLoggedUser,
+  editProfile,
+  getUserById,
+  onlyAdminRead,
+  deleteAccount,
+} = require("../controllers/user.controller");
+//vamos a importar los middleware
+const { verifyToken, checkRole } = require("../middleware");
 
 //Read - perfil
-router.get("/my-profile",verifyToken,getLoggedUser);
+router.get("/my-profile", verifyToken, getLoggedUser);
+
 //UPDate - Perfil
-router.patch("/edit-profile",verifyToken,editProfile);
+router.patch("/edit-profile", verifyToken, editProfile);
+
 //Delete - user
-//router.delete("/delete-user");
+router.delete("/delete-user", verifyToken, deleteAccount);
 
 //Read - otro usuario
-//router.get("/:id/profile")
+router.get("/:id/profile", verifyToken, getUserById);
 
+// Read all user!!! (debe de ir en admin staff)
+router.get("/admin/users", verifyToken, checkRole(['Admin']), onlyAdminRead );
 
 module.exports = router;
